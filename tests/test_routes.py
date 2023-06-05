e8b9e1ee1a561269f5fe61931ed35d32cd00de25
Deivid Fae Viana <deividfae@yahoo.com.br>
2023-06-05 10:19:51 -0400

    CORS added

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
-from service import tasliman
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -34,7 +34,7 @@ class TestAccountService(TestCase):
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
-        tasliman.force_https = False
+        talisman.force_https = False
         init_db(app)
 
     @classmethod
@@ -185,3 +185,10 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+
+    def test_cors_security(self):
+        """It should return a CORS hearde"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
